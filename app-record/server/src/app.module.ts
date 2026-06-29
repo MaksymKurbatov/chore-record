41807992b70a3fb743249f541b6b73203ad43209
maxk7074 <maxk7074@gmail.com>
2026-06-29 17:56:45 +0300

    replayce prisma to server folder, add swagger

--- app-record/server/src/app.module.ts
+++ app-record/server/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaService } from './prisma.service'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [PrismaService],
+  imports: [AuthModule]
 })
 export class AppModule {}
