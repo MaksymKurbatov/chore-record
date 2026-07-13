72f953a2369f7e3cdbab617e8a56da6f68ed2ba3
maxk7074 <maxk7074@gmail.com>
2026-07-13 15:39:58 +0300

    implement products

--- app-record/server/src/product/product.controller.ts
+++ app-record/server/src/product/product.controller.ts
@@ -1,7 +1,55 @@
-import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Query,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { ProductService } from './product.service'
+import { ProductDto } from './dto/product.dto'
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAll(@Query('searchTerm') searchTerm?: string) {
+    return this.productService.getAll(searchTerm)
+  }
+
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.productService.getBySlug(slug)
+  }
+
+  @Get('by-category/:category')
+  async getByCategory(@Param('category') category: string) {
+    return this.productService.getByCategory(category)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  async create() {
+    return await this.productService.create()
+  }
+
+  @HttpCode(200)
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() dto: ProductDto) {
+    return await this.productService.update(id, dto)
+  }
+
+  @HttpCode(200)
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productService.delete(id)
+  }
 }
--- app-record/server/src/user/user.service.ts
+++ app-record/server/src/user/user.service.ts
@@ -1,20 +1,61 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from '../prisma/prisma.service'
+import { Prisma } from '../../generated/prisma/client'
+import { returnUserObject } from './return-user-object'
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getProfile(id: string) {
+    return this.getByID(id)
+  }
+
+  async getByID(id: string, selectObject: Prisma.UserSelect = {}) {
     const user = await this.prisma.user.findUnique({
       where: { id },
-      omit: { password: true }
+      select: {
+        ...returnUserObject,
+        favorites: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            image: true,
+            slug: true,
+            category: {
+              select: {
+                name: true
+              }
+            }
+          }
+        },
+        ...selectObject
+      }
     })
-
     if (!user) {
       throw new NotFoundException('User not found')
     }
-
     return user
+  }
+
+  async toggleFavorite(userId: string, productId: string) {
+    const user = await this.getByID(userId)
+    if (!user) throw new NotFoundException('User not found')
+
+    const isExist = user.favorites.some(product => product.id === productId)
+
+    await this.prisma.user.update({
+      where: {
+        id: user.id
+      },
+      data: {
+        favorites: {
+          [isExist ? 'disconnect' : 'connect']: {
+            id: productId
+          }
+        }
+      }
+    })
   }
 }
