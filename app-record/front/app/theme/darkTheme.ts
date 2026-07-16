cacab6e17f62e8c457fe501ff9e4a2bd7c7eb983
maxk7074 <maxk7074@gmail.com>
2026-07-16 16:27:59 +0300

    implement native paper and them

---
+++ app-record/front/app/theme/darkTheme.ts
@@ -0,0 +1,13 @@
+import { MD3DarkTheme } from 'react-native-paper'
+
+export const darkTheme = {
+	...MD3DarkTheme,
+	colors: {
+		...MD3DarkTheme.colors,
+		primary: '#03dac6',
+		secondary: '#03dac6',
+		background: '#121212',
+		surface: '#1e1e1e',
+		text: '#ffffff'
+	}
+}
---
+++ app-record/front/app/theme/lightTheme.ts
@@ -0,0 +1,16 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+
+
+
+export const lightTheme = {
+	...MD3LightTheme,
+	colors: {
+		...MD3LightTheme.colors,
+		primary: '#6200ee',
+		secondary: '#121212',
+		background: '#f6f6f6',
+		surface: '#ffffff',
+		text: '#000000'
+	}
+}
